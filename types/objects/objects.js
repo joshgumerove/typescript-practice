88faf69a2f4c720dca0c90405d2822f42f5f4d9c
Josh Gumerove <jgumerove@kaufmanrossin.com>
2022-12-02 14:33:50 -0500

    excess properties

--- types/objects/objects.js
+++ types/objects/objects.js
@@ -12,3 +12,23 @@ var printName = function (name) {
 };
 // note how the above accepts an object
 console.log(printName({ first: "Josh", last: "Gumerove" }));
+// more on objects
+var coordinate = {
+    x: 34,
+    y: 2
+}; // this is not usually done but is valid
+function makeRandomCoordinate(x, y) {
+    return { x: x, y: y };
+}
+console.log(makeRandomCoordinate(5, 9));
+function randomCoordinate() {
+    return { x: Math.random(), y: Math.random() };
+}
+// Excess properties
+// printName({ first: "Mick", last: "Jagger", age: 55 }); // note how we added an age -- cannot do this
+var singer = {
+    first: "Mick",
+    last: "Jagger",
+    age: 55
+};
+console.log(printName(singer));
--- types/objects/objects.ts
+++ types/objects/objects.ts
@@ -16,3 +16,31 @@ const printName = (name: { first: string; last: string }): string => {
 // note how the above accepts an object
 
 console.log(printName({ first: "Josh", last: "Gumerove" }));
+
+// more on objects
+
+let coordinate: { x: number; y: number } = {
+  x: 34,
+  y: 2,
+}; // this is not usually done but is valid
+
+function makeRandomCoordinate(x: number, y: number): { x: number; y: number } {
+  return { x: x, y: y };
+}
+
+console.log(makeRandomCoordinate(5, 9));
+
+function randomCoordinate(): { x: number; y: number } {
+  return { x: Math.random(), y: Math.random() };
+}
+
+// Excess properties
+
+// printName({ first: "Mick", last: "Jagger", age: 55 }); // note how we added an age -- cannot do this
+const singer = {
+  first: "Mick",
+  last: "Jagger",
+  age: 55,
+};
+
+console.log(printName(singer)); // note how we do not get an error here -- decision made early on with typescript (typescript only checks to see if certain properties are there)
