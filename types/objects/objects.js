702cb137f3dc04f1ad45091f39c2674d8258d18a
Josh Gumerove <jgumerove@kaufmanrossin.com>
2022-12-08 16:10:51 -0500

    nested objects

--- types/objects/objects.js
+++ types/objects/objects.js
@@ -13,17 +13,17 @@ var printName = function (name) {
 // note how the above accepts an object
 console.log(printName({ first: "Josh", last: "Gumerove" }));
 // more on objects
-var coordinate = {
-    x: 34,
-    y: 2
-}; // this is not usually done but is valid
+// let coordinate: { x: number; y: number } = {
+//   x: 34,
+//   y: 2,
+// }; // this is not usually done but is valid
 function makeRandomCoordinate(x, y) {
     return { x: x, y: y };
 }
 console.log(makeRandomCoordinate(5, 9));
-function randomCoordinate() {
-    return { x: Math.random(), y: Math.random() };
-}
+// function randomCoordinate(): { x: number; y: number } {
+//   return { x: Math.random(), y: Math.random() };
+// }
 // Excess properties
 // printName({ first: "Mick", last: "Jagger", age: 55 }); // note how we added an age -- cannot do this
 var singer = {
@@ -31,4 +31,39 @@ var singer = {
     last: "Jagger",
     age: 55
 };
-console.log(printName(singer));
+console.log(printName(singer)); // note how we do not get an error here -- decision made early on with typescript (typescript only checks to see if certain properties are there)
+// type aliases
+var coordinate = {
+    x: 34,
+    y: 2
+};
+function randomCoordinate() {
+    return { x: Math.random(), y: Math.random() };
+}
+function doublePoint(point) {
+    return {
+        x: point.x * 2,
+        y: point.y * 2
+    };
+}
+var sayHappyBirthday = function (person) {
+    return "Hey ".concat(person.name, " congrats on turning ").concat(person.age);
+};
+console.log(sayHappyBirthday({ name: "Josh Gumerove", age: 32 }));
+function calculatePayout(song) {
+    return song.numbStreams * 0.0033;
+}
+function printSongDetails(song) {
+    console.log(song.title, song.artist);
+}
+var test = {
+    title: "unclaimed",
+    artist: "stuff",
+    numbStreams: 1232,
+    credits: {
+        producer: "phiilll something",
+        writer: "bill jean"
+    }
+};
+console.log(calculatePayout(test));
+printSongDetails(test);
--- types/objects/objects.ts
+++ types/objects/objects.ts
@@ -19,10 +19,10 @@ console.log(printName({ first: "Josh", last: "Gumerove" }));
 
 // more on objects
 
-let coordinate: { x: number; y: number } = {
-  x: 34,
-  y: 2,
-}; // this is not usually done but is valid
+// let coordinate: { x: number; y: number } = {
+//   x: 34,
+//   y: 2,
+// }; // this is not usually done but is valid
 
 function makeRandomCoordinate(x: number, y: number): { x: number; y: number } {
   return { x: x, y: y };
@@ -30,9 +30,9 @@ function makeRandomCoordinate(x: number, y: number): { x: number; y: number } {
 
 console.log(makeRandomCoordinate(5, 9));
 
-function randomCoordinate(): { x: number; y: number } {
-  return { x: Math.random(), y: Math.random() };
-}
+// function randomCoordinate(): { x: number; y: number } {
+//   return { x: Math.random(), y: Math.random() };
+// }
 
 // Excess properties
 
@@ -44,3 +44,74 @@ const singer = {
 };
 
 console.log(printName(singer)); // note how we do not get an error here -- decision made early on with typescript (typescript only checks to see if certain properties are there)
+
+// type aliases
+
+let coordinate: { x: number; y: number } = {
+  x: 34,
+  y: 2,
+};
+
+type Point = {
+  x: number;
+  y: number;
+}; // not creating a new type just creating a reference to a type
+
+function randomCoordinate(): Point {
+  return { x: Math.random(), y: Math.random() };
+}
+
+function doublePoint(point: Point): Point {
+  return {
+    x: point.x * 2,
+    y: point.y * 2,
+  };
+}
+
+// instead of writing out object typs in an annotation - can declare them separaretly in a type alias
+
+type Person = {
+  name: string;
+  age: number;
+}; // convention to be capital but does not have to be
+
+const sayHappyBirthday = (person: Person): string => {
+  return `Hey ${person.name} congrats on turning ${person.age}`;
+};
+
+console.log(sayHappyBirthday({ name: "Josh Gumerove", age: 32 }));
+
+// does not have to be an object type but usually is
+
+// nested object types
+
+type Song = {
+  title: string;
+  artist: string;
+  numbStreams: number;
+  credits: {
+    producer: string;
+    writer: string;
+  };
+};
+
+function calculatePayout(song: Song): number {
+  return song.numbStreams * 0.0033;
+}
+
+function printSongDetails(song: Song): void {
+  console.log(song.title, song.artist);
+}
+
+const test = {
+  title: "unclaimed",
+  artist: "stuff",
+  numbStreams: 1232,
+  credits: {
+    producer: "phiilll something",
+    writer: "bill jean",
+  },
+};
+
+console.log(calculatePayout(test));
+printSongDetails(test);
