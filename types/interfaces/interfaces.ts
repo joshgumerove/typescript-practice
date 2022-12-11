d9203b5b740cc9c5f9045d96e1814fcd4afab0ff
Josh Gumerove <jgumerove@kaufmanrossin.com>
2022-12-11 18:02:47 -0500

    interface methods

--- types/interfaces/interfaces.ts
+++ types/interfaces/interfaces.ts
@@ -6,6 +6,8 @@
 //   y: number;
 // };
 
+export {};
+
 interface Point {
   x: number;
   y: number;
@@ -19,3 +21,58 @@ interface Person {
   name: string;
   age: number;
 }
+
+// can also create option properties
+
+interface Person1 {
+  readonly id: number;
+  firstName: string;
+  lastName: string;
+  nickName?: string;
+}
+
+const thomas: Person1 = {
+  id: 55555,
+  firstName: "Thomas",
+  lastName: "Hardy",
+  nickName: "Tommy",
+};
+
+thomas.firstName = "Tommy John";
+// thomas.id = 55555555; cannot change because is readonly
+
+// interface methods -- can describe the shape of an object in terms of methods should include and what should return
+
+interface Individual {
+  readonly id: number;
+  first: string;
+  last: string;
+  nickname?: string;
+  sayHi: () => string; // one way of writing this -- saying must be a method that returns string
+  // sayHi(): string -- another way to define
+}
+
+const josh: Individual = {
+  id: 556234234,
+  first: "Josh",
+  last: "Gumerov",
+  sayHi: () => `Hello ${josh.first}`,
+};
+
+console.log(josh.sayHi());
+
+interface Product {
+  name: string;
+  price: number;
+  applyDiscount(discount: number): number;
+}
+
+const shoes: Product = {
+  name: "blue suede shoes",
+  price: 100,
+  applyDiscount(discount: number): number {
+    return shoes.price - discount * shoes.price;
+  },
+};
+
+console.log(shoes.applyDiscount(0.3));
