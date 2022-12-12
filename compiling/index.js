f83646c8fc6141375c63a74ed1c12e7982f0bb6f
Josh Gumerove <jgumerove@kaufmanrossin.com>
2022-12-12 17:49:27 -0500

    compiling multiple files

---
+++ compiling/index.js
@@ -0,0 +1,9 @@
+"use strict";
+const norma = {
+    breed: "Silkie",
+    eggsPerWeek: 4,
+    name: "Norma",
+    age: 4,
+};
+// note tsc -w file_name command
+// ntoe tsc --init command
--- compiling/index.ts
+++ compiling/index.ts
@@ -2,4 +2,15 @@ interface Chicken {
   breed: string;
   eggsPerWeek: number;
   name: string;
+  age: number;
 }
+
+const norma: Chicken = {
+  breed: "Silkie",
+  eggsPerWeek: 4,
+  name: "Norma",
+  age: 4,
+};
+
+// note tsc -w file_name command
+// ntoe tsc --init command
